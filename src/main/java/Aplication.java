2cda54f92aad87902ed0d97fd2fa8ab0b13b0eec
KreSeb <s.krezel@outlook.com>
2019-03-03 01:05:17 +0100

    First Commit

---
+++ src/main/java/Aplication.java
@@ -0,0 +1,9 @@
+import com.sun.javafx.css.CalculatedValue;
+
+public class Aplication {
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.adding(3,6));
+        System.out.println(calculator.substraction(10,2));
+    }
+}
